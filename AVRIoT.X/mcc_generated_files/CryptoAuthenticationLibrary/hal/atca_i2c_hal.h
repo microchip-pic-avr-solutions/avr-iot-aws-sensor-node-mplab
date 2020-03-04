f7406330627fdab1874c84e3b4a7c013499ecb84
Microchip Technology <microchip@microchip.com>
2020-03-04 19:20:26 +0000

    Merging into the (develop) branch

---
+++ AVRIoT.X/mcc_generated_files/CryptoAuthenticationLibrary/hal/atca_i2c_hal.h
@@ -0,0 +1,24 @@
+
+#ifndef ATCA_I2C_HAL_H_
+#define ATCA_I2C_HAL_H_
+
+/** \defgroup hal_ Hardware abstraction layer (hal_)
+ *
+ * \brief
+ * These methods define the hardware abstraction layer for communicating with a CryptoAuth device
+ * using I2C driver of ASF.
+ *
+   @{ */
+
+ATCA_STATUS hal_i2c_init(void *hal, ATCAIfaceCfg *cfg);
+ATCA_STATUS hal_i2c_post_init(ATCAIface iface);
+ATCA_STATUS hal_i2c_send(ATCAIface iface, uint8_t *txdata, int txlength);
+ATCA_STATUS hal_i2c_receive(ATCAIface iface, uint8_t *rxdata, uint16_t *rxlength);
+ATCA_STATUS hal_i2c_wake(ATCAIface iface);
+ATCA_STATUS hal_i2c_idle(ATCAIface iface);
+ATCA_STATUS hal_i2c_sleep(ATCAIface iface);
+ATCA_STATUS hal_i2c_release(void *hal_data);
+
+
+/** @} */
+#endif /* ATCA_I2C_HAL_H_ */
